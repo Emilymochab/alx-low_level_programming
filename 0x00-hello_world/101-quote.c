457418ac64c8b76e79cf2374459156c1a6fe00e4
Emilymochab <lesegomochabang0@gmail.com>
2022-06-03 05:04:44 -0700

    101 quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
-#include <stdlib.h>
-
 /**
  * main - Entry point
- * Return: Always 1 (Success)
+ *
+ * Return : Always 0 (Success)
  */
 int main(void)
 {
-	write(STDOUT_FILENO, "and that piece of art is useful
-			\" - Dora Korpar, 2015-10-19\n", 59);
+	char *quote = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	Write(2, quote, 59);
 	return (1);
 }
