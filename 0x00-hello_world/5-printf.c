5471edcaa10e22936500f01ab3d7fb9c8c15edaf
Emilymochab <lesegomochabang0@gmail.com>
2022-06-03 04:36:30 -0700

    5 printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,16 +6,6 @@
  */
 int main(void)
 {
-	char a;
-	int b;
-	long c;
-	long long d;
-	float e;
-
-	printf("Size of a char: %ld byte(s)\n", sizeof(a));
-	printf("Size of an int: %ld byte(s)\n", sizeof(b));
-	printf("Size of a long int: %ld byte(s)\n", sizeof(c));
-	printf("Size of a long long int: %ld byte(s)\n", sizeof(d));
-	printf("Size of a float: %ld byte(s)\n", sizeof(e));
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
