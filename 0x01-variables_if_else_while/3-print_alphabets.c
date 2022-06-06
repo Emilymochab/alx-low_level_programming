320be9c856096a1ddf1247925866bff1baddf4bc
Emilymochab <lesegomochabang0@gmail.com>
2022-06-06 12:48:06 -0700

    3 prints alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,19 +2,36 @@
 
 /**
  *
- * main - Entry point
+ *   * main - Entry point
  *
- * Return: 0 for sucess
- */
+ *     *
+ *
+ *       * Return: 0 for sucess
+ *
+ *         */
+
+
 
 int main(void)
 
+
+
 {
-	char alphabet;
-	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
-			putchar(alphabet);
-		for (alphabet = 'A'; alphabet <= 'Z'; alphabet++)
-			putchar(alphabet);
-			putchar('\n');
-	return (0);
+
+		char alphabet;
+
+				for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+
+								putchar(alphabet);
+
+					for (alphabet = 'A'; alphabet <= 'Z'; alphabet++)
+
+								putchar(alphabet);
+
+						putchar('\n');
+
+
+
+							return (0);
+
 }
