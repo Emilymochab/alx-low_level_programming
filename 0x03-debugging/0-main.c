6ccf1ce5fbb8d47bb4af5aa6305b8298af15bf09
Emilymochab <lesegomochabang0@gmail.com>
2022-06-09 10:56:20 -0700

    1 main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - tests function that prints if integer is positive or negative
+ * main - main
  *
  * Return: 0
  */
@@ -9,8 +9,8 @@
 int main(void)
 {
 int i;
+i = 0;
 
-i = 98;
 positive_or_negative(i);
 
 return (0);
--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -22,4 +22,5 @@ i = 0;
 */
 printf("Infinite loop avoided! \\o/\n");
 return (0);
+
 }
