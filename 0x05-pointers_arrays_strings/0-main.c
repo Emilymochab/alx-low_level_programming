17d56c6f7cc3de272cba6b6e62d33dac946551f5
Emilymochab <lesegomochabang0@gmail.com>
2022-06-14 04:11:26 -0700

    0 reset to 98

--- 0x05-pointers_arrays_strings/0-main.c
+++ 0x05-pointers_arrays_strings/0-main.c
@@ -11,10 +11,10 @@ int main(void)
 
 	 int n;
 
-	 n = 402;
-	 printf("n=%d\n", n);
-	 reset_to_98(&n);
-	 printf("n=%d\n", n);
-	 return (0);
+        n = 402;
+	printf("n=%d\n", n);
+	reset_to_98(&n);
+	printf("n=%d\n", n);
+	return (0);
 
 }
