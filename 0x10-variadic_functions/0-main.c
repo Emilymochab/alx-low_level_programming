f49754162859168b18e599f49f63e458d465441c
Emilymochab <lesegomochabang0@gmail.com>
2022-07-07 10:13:47 -0700

    0-sum_them_all.c

--- 0x10-variadic_functions/0-main.c
+++ 0x10-variadic_functions/0-main.c
@@ -7,10 +7,9 @@
  * Return: Always 0.
  */
 int main(void)
-
 {
-       	int sum;
-
+	int sum;
+       
 	sum = sum_them_all(2, 98, 1024);
 	printf("%d\n", sum);
 	sum = sum_them_all(4, 98, 1024, 402, -1024);
